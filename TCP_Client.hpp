5c53bb7532bba7a141f7ab2ae9def79ca7ae8841
AlvinLIQ <alvinliq@outlook.com>
2025-02-25 10:01:15 +0000

    Go header only mode

--- TCP_Client.hpp
+++ TCP_Client.hpp
@@ -8,14 +8,62 @@ namespace TCP
 	class TCP_Client
 	{
 	public:
-		TCP_Client(const SOCKET& c_fd);
-		TCP_Client(const char* host, int port);
-		~TCP_Client();
-		int Connect();
-		int Recv(const int bufSize = TCP_BUFFER_SIZE);
-		int Send(const char* data, int len);
-
-		int Close();
+		TCP_Client(const SOCKET& c_fd)
+		{
+			s_fd = c_fd;
+			state = ConnectionStates::Connected;
+		}
+		
+		TCP_Client(const char* host, int port)
+		{
+			s_fd = Socket::initSocket();
+			sockAddr = Socket::initAddr(host, port);
+		}
+		
+		~TCP_Client()
+		{
+		}
+		
+		int Connect()
+		{
+			int result = Socket::sockConn(&s_fd, &sockAddr);
+			if (!result)
+			{
+				ioctlsocket(s_fd, FIONBIO, (u_long*)&Socket::OptVal);
+				state = ConnectionStates::Connected;
+			}
+			else
+				state = ConnectionStates::Disconnected;
+		
+			return result;
+		}
+		
+		int Recv(const int bufSize)
+		{
+			int len = recv(s_fd, buf, bufSize, 0);
+			if (!len || Socket::SocketShouldClose())
+				Close();
+			bufLen = len <= 0 ? 0 : len;
+			buf[bufLen] = '\0';
+		
+			return len;
+		}
+		
+		int Send(const char* data, int len)
+		{
+			int result = send(s_fd, data, len, 0);
+			if (Socket::SocketShouldClose())
+				Close();
+		
+			return result;
+		}
+		
+		int Close()
+		{
+			closesocket(s_fd);
+			state = ConnectionStates::Closed;
+			return 0;
+		}
 
 		const char* GetBuffer()
 		{
--- TCP_Server.hpp
+++ TCP_Server.hpp
@@ -12,13 +12,82 @@ namespace TCP
 	class TCP_Server
 	{
 	public:
-		TCP_Server(const char* ip, int port);
-		~TCP_Server();
-
-		void Listen(void(*ConnectionCallback)(TCP_Client* client), bool sync = false);
-		void Stop();
-
-		static void ExitSignal();
+		TCP_Server(const char* ip, int port)
+		{
+			s_fd = Socket::initSocket();
+			serverAddr = Socket::initAddr(ip, port);
+		}
+		
+		~TCP_Server()
+		{
+			Stop();
+			if (pServerThread != nullptr)
+			{
+				if (pServerThread->joinable())
+					pServerThread->join();
+				delete pServerThread;
+				pServerThread = nullptr;
+			}
+			closesocket(s_fd);
+			while (!connections.empty())
+			{
+				connections.begin()->Close();
+				connections.erase(connections.begin());
+			}
+		}
+		void Listen(void(*ConnectionCallback)(TCP_Client* client), bool sync)
+		{
+			if (!sync)
+			{
+				if (pServerThread != nullptr)
+				{
+					if (pServerThread->joinable())
+						pServerThread->join();
+					delete pServerThread;
+					pServerThread = nullptr;
+				}
+				pServerThread = new std::thread([this, ConnectionCallback]()
+				{
+					Listen(ConnectionCallback, true);
+				});
+				return;
+			}
+			setsockopt(s_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&Socket::OptVal, sizeof(char));
+			ioctlsocket(s_fd, FIONBIO, (u_long*)&Socket::OptVal);
+			socklen_t addrLen = sizeof(serverAddr);
+			if (Socket::listenSocket(s_fd, &serverAddr, addrLen) == -1)
+				return;
+			
+			state = ServerStates::Listening;
+			SOCKET c_fd;
+			while (state == ServerStates::Listening)
+			{
+				while (connections.size() < MaxClientCount && (c_fd = accept(s_fd, (struct sockaddr*)&serverAddr, &addrLen)) != (SOCKET)-1)
+				{
+					connections.push_back(TCP_Client(c_fd));
+					//std::thread([ConnectionCallback, this, conn = connections.rbegin()]()
+					//{
+					//ConnectionCallback(c_fd);
+						//connections.erase(conn.base());
+					//}).detach();
+				}
+				for (auto conn = connections.begin(); conn < connections.end(); conn++)
+				{
+					ConnectionCallback((TCP_Client*)&(*conn));
+					if (conn->GetState() != Connected)
+					{
+						connections.erase(conn);
+						break;
+					}
+				}
+			}
+			state = ServerStates::Stopped;
+		}
+		
+		void Stop()
+		{
+			state = ServerStates::Stopped;
+		}
 
 		const ServerStates GetConnectionState()
 		{
